bcffa17492029a656f812d5baf568be698994654
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-11-14 15:41:07 -0300

    Day 01 Done :)

---
+++ Day001_1/Class12 - Input Function/main.py
@@ -0,0 +1,6 @@
+print(len(input("What is your name? ")))
+
+# Notes
+# if input was "Hytally"
+# 1st: print(len("Hytally"))
+# 2nd: print(7)
---
+++ Day001_1/Class15 - Variable Naming/main.py
@@ -0,0 +1,8 @@
+name = "Jack"
+print(name)
+
+nama = "Angela"
+print(nama)
+
+user_name = input("What is your name? ")
+print(user_name)
