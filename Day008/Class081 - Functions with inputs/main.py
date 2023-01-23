e69d7e3883665463b88d5ed847ee46ff06d2b1c8
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2023-01-23 08:05:30 -0300

    Day 08 Done!

---
+++ Day008/Class081 - Functions with inputs/main.py
@@ -0,0 +1,20 @@
+#Review:
+# Create a Function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet():
+#     print("Hi, my name is Lucas.")
+#     print("I have 28 years old.")
+#     print("Nice to meet you!!!")
+
+# greet()
+
+# Function that allows for input
+
+def greet_with_name(name):
+    print(f"Hi my name is {name}")
+    print(f"{name} has 28 years old.")
+
+
+greet_with_name("Lucas")
---
+++ Day008/Class082 - Positional vs Keyword Arguments/main.py
@@ -0,0 +1,9 @@
+#Function with more than 1 input
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+# greet_with("Lucas", "Anchieta")
+
+greet_with(location = "Anchieta", name = "Lucas")
